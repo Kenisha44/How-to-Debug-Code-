553db7bbe107dc1cde69ce143e2c4887b9f62400
Kenisha Johnson <kenishaajohnson@gmail.com>
2022-06-25 17:56:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#debugging 
+
+#linting 
+#ide/editor 
+# read errors 
+#pdb - python debugger built in module
+
+import pdb 
+def add(num1, num2): 
+  pdb.set_trace()
+  return num1 + num2
+
+add(4, 'djfisd')
+
+#type in help 
+#help list for more information regarding commands for list 
+#step useful command 
+#a gives all the arguments 
+#w shows context of current line executed 
